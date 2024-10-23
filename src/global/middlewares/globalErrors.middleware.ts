c0a44a41a9a763de3d5cd98e229cd8765d2da325
boodyshlby55 <boodyshlby55@gmail.com>
2024-10-23 17:41:12 +0300

    jwt error

--- src/global/middlewares/globalErrors.middleware.ts
+++ src/global/middlewares/globalErrors.middleware.ts
@@ -1,20 +1,31 @@
 import express from 'express';
+import ApiErrors from "../utils/apiErrors";
 import {CustomErrors} from '../interfaces/customErrors.interface';
 
-const globalErrors = (err: CustomErrors, req: express.Request, res: express.Response, next: express.NextFunction) => {
+const devErrors = (err: CustomErrors, res: express.Response) =>
+    res.status(err.statusCode!).json({
+        error: err,
+        message: err.message,
+        stack: err.stack
+    });
+
+const prodErrors = (err: CustomErrors, res: express.Response) =>
+    res.status(err.statusCode!).json({
+        status: err.status,
+        message: err.message
+    });
+
+const handleJwtExpired = (message: string) => new ApiErrors(message, 401);
+
+const globalErrors = (err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
     err.statusCode = err.statusCode || 500;
     err.status = err.status || 'Error';
     if (process.env.NODE_ENV === 'development') {
-        res.status(err.statusCode).json({
-            error: err,
-            message: err.message,
-            stack: err.stack
-        });
+        if (err.name === 'TokenExpiredError' || err.name === 'JsonWebTokenError') err = handleJwtExpired(`${req.__('session_expired')}`);
+        devErrors(err, res);
     } else {
-        res.status(err.statusCode).json({
-            status: err.status,
-            message: err.message
-        });
+        if (err.name === 'TokenExpiredError' || err.name === 'JsonWebTokenError') err = handleJwtExpired(`${req.__('session_expired')}`);
+        prodErrors(err, res);
     }
 };
 
