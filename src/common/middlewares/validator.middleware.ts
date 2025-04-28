ad932a5217f43c234ac598c6a0626e0e51cd7529
abdelrahman-sherif55 <boodyshlby55@gmail.com>
2025-04-28 22:21:54 +0300

    updates

--- src/common/middlewares/validator.middleware.ts
+++ src/common/middlewares/validator.middleware.ts
@@ -1,11 +1,12 @@
 import {NextFunction, Request, RequestHandler, Response} from 'express';
 import {validationResult} from 'express-validator';
+import {HttpStatusCode} from "../enums/status-code.enum";
 
 const validatorMiddleware: RequestHandler = (req: Request, res: Response, next: NextFunction) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(400).json({
-      statusCode: 400,
+    return res.status(HttpStatusCode.BAD_REQUEST).json({
+      statusCode: HttpStatusCode.BAD_REQUEST,
       status: 'Bad Request',
       message: errors.array()[0].msg,
     });
